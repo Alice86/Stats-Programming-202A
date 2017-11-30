57521f2f5fca0440f2ee785f1aee99ca63b054bb
JiayuWu <wujiayu86@outlook.com>
2017-11-29 20:46:31 -0800

    fix annotation of package generating codes

--- package.R
+++ package.R
@@ -1,20 +1,24 @@
+## How to make a package in R
+
+# import packages
 library(roxygen2)
 library(devtools)
 
+# creat the project
 setwd("/Users/alice/Documents/R/StatsProgramming_202A")
 create("LMjw")
-
+# if the package requires rcpp
 library(Rcpp)
 library(RcppArmadillo)
 RcppArmadillo.package.skeleton("JWlm",force=T)
 
+# edit and save codes
+
+# export functions and generate documents
 setwd("/Users/alice/Documents/R/StatsProgramming_202A/LMjw")
 document()
 compileAttributes(verbose=TRUE)
 
+# install and try
 setwd("/Users/alice/Documents/R/StatsProgramming_202A")
 install("LMjw")
-
-library(Rcpp)
-library(RcppArmadillo)
-RcppArmadillo.package.skeleton("LMjw")
